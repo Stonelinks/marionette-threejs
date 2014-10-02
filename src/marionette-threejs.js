66c927b0d0e47a31768479739a5cd64d74b8ebdc
Lucas Doyle <lucas.p.doyle@gmail.com>
2014-10-02 14:46:26 +0900

    fix Marionette and threejs dependency in shim

--- src/marionette-threejs.js
+++ src/marionette-threejs.js
@@ -4,25 +4,27 @@
 
   // Set up m3js appropriately for the environment. Start with AMD.
   if (typeof define === 'function' && define.amd) {
-    define(['backbone', 'exports'], function(Backbone, exports) {
+    define(['backbone', 'marionette', 'threejs', 'exports'], function(Backbone, Marionette, THREE, exports) {
       // Export global even in AMD case in case this script is loaded with
       // others that may still expect a global m3js.
-      root.m3js = factory(root, exports, Backbone);
+      root.m3js = factory(root, exports, Backbone, Marionette, THREE);
     });
 
     // Next for Node.js or CommonJS. jQuery may not be needed as a module.
   }
   else if (typeof exports !== 'undefined') {
     var Backbone = require('backbone');
-    factory(root, exports, Backbone);
+    var Marionette = require('marionette');
+    var THREE = require('threejs');
+    factory(root, exports, Backbone, Marionette, THREE);
 
     // Finally, as a browser global.
   }
   else {
-    root.m3js = factory(root, {}, root.Backbone);
+    root.m3js = factory(root, {}, root.Backbone, root.Marionette, root.THREE);
   }
 
-}(this, function(root, m3js, Backbone) {
+}(this, function(root, m3js, Backbone, Marionette, THREE) {
   'use strict';
 
   // #Bundle modified threejs controls
